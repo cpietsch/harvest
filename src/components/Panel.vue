b64a53476a4782988fa44b4c43a6cf64f7843480
Christopher Pietsch <cpietsch@gmail.com>
2017-09-11 16:07:31 +0200

    Added simple panel

---
+++ src/components/Panel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="panel">
+    <div v-bind:style="{ width: width + 'px', height: height + 'px' }">
+      <h3>{{ title }}</h3>
+      <h4>{{ value }} {{ unit }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'panel',
+  props: ['value', 'width', 'height', 'title', 'unit'],
+  computed: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+  .panel {
+    padding: 20px;
+    margin: 20px;
+    background: #151515;
+    display: block;
+    float: left;
+  }
+
+  h3 {
+    text-transform: uppercase;
+    font-size: 1.4em;
+    color: #696969;
+    margin: 0;
+    margin-bottom: 10px;
+  }
+  h4 {
+    font-size: 1.3em;
+    font-weight: normal;
+    color: #585858;
+    margin: 0;
+    margin-bottom: 10px;
+  }
+
+</style>
