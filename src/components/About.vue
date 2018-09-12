0bac4475af3fc9c2979a012688c973a4803b6213
Christopher Pietsch <cpietsch@gmail.com>
2018-09-12 19:26:33 -0400

    updated ZEC address

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,19 +6,19 @@
         This is the endpoint of the realtime projection for Julian Olivers <a href="https://julianoliver.com/output/harvest" target="_blank">HARVEST</a> installation.
       </p>
       <p>
-        There are 2 views: <a href="#/vis/t1YR6ihrgmWWGp4kDjyeXRpbUgEUzmFDSVC/graph">hashrate history</a> & <a href="#/vis/t1YR6ihrgmWWGp4kDjyeXRpbUgEUzmFDSVC/dashboard">dashboard</a> both are optimized for full hd. To display your own miner just change the ZEC address in the url to yours. You can find the source code on <a href="https://github.com/cpietsch/harvest" target="_blank">GitHub</a>.
+        There are 2 views: <a href="#/vis/31o2tiFd3anrB7KWLhNvCFyx9WnxAWkZUr/graph">hashrate history</a> & <a href="#/vis/31o2tiFd3anrB7KWLhNvCFyx9WnxAWkZUr/dashboard">dashboard</a> both are optimized for full hd. To display your own miner just change the ZEC address in the url to yours. You can find the source code on <a href="https://github.com/cpietsch/harvest" target="_blank">GitHub</a>.
       </p>
       <p>
         APIs used: <a href="https://zec.nanopool.org/api" target="_blank">Nanopool</a>, <a href="https://min-api.cryptocompare.com" target="_blank">Cryptocompare</a>
       </p>
       <p>
-        Created by <a href="https://chrispie.com" target="_blank">Christopher Pietsch</a> 2017
+        Created by <a href="https://chrispie.com" target="_blank">Christopher Pietsch</a> 2017-2018
       </p>
 
     </div>
     <div>
-      <dashboard pollingTime="30000" polling="true" addr="t1YR6ihrgmWWGp4kDjyeXRpbUgEUzmFDSVC" class="scaledDashboard" />
-      <graph pollingTime="30000" polling="true" addr="t1YR6ihrgmWWGp4kDjyeXRpbUgEUzmFDSVC" class="scaledGraph" />
+      <dashboard pollingTime="30000" polling="true" addr="31o2tiFd3anrB7KWLhNvCFyx9WnxAWkZUr" class="scaledDashboard" />
+      <graph pollingTime="30000" polling="true" addr="31o2tiFd3anrB7KWLhNvCFyx9WnxAWkZUr" class="scaledGraph" />
     </div>
   </div>
 </template>
